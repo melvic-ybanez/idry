4600f2a48edc92342a58e76265b51eed618cad10
Melvic Ybanez <melvic@hubbub.ai>
2023-10-21 04:06:06 +0800

    Scan two-character punctuations

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,5 +1,6 @@
+use std::thread::current;
 use crate::common::Line;
-use crate::tokens::{Token, TokenType};
+use crate::tokens::{Bitwise, Comparison, Keyword, Token, TokenType};
 use crate::tokens::Arithmetic;
 
 pub struct Scanner {
@@ -32,6 +33,13 @@ impl Scanner {
                 '%' => self.make_token(TokenType::Arithmetic(Arithmetic::Modulo)),
                 '-' => self.make_token(TokenType::Arithmetic(Arithmetic::Minus)),
                 ';' => self.make_token(TokenType::Semicolon),
+                '!' => self.make_token_or_else(
+                    '=', TokenType::Comparison(Comparison::NotEqual), TokenType::Keyword(Keyword::Not)),
+                '=' => self.make_token_or_else(
+                    '=', TokenType::Comparison(Comparison::EqualEqual), TokenType::Comparison(Comparison::Equal)),
+                '&' => self.make_token(TokenType::Bitwise(Bitwise::BAnd)),
+                '|' => self.make_token(TokenType::Bitwise(Bitwise::BOr)),
+                '^' => self.make_token(TokenType::Bitwise(Bitwise::BXor)),
                 _ => Token::error("Unexpected character.", self)
             }
         }
@@ -40,6 +48,25 @@ impl Scanner {
     fn advance(&mut self) -> char {
         self.current += 1;
         self.source.as_bytes()[self.current - 1] as char
+    }
+
+    fn match_char(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            false
+        } else if self.current_char() != expected {
+            false
+        } else {
+            self.current += 1;
+            true
+        }
+    }
+
+    fn current_char(&self) -> char {
+        self.source_at(self.current)
+    }
+
+    fn source_at(&self, index: usize) -> char {
+        self.source.as_bytes()[index] as char
     }
 
     fn is_at_end(&self) -> bool {
@@ -62,7 +89,12 @@ impl Scanner {
         self.source.as_str()
     }
 
-    pub fn make_token(&self, token_type: TokenType) -> Token {
+    fn make_token(&self, token_type: TokenType) -> Token {
         Token::new(token_type, self)
+    }
+
+    fn make_token_or_else(&mut self, expected: char, if_match: TokenType, or_else: TokenType) -> Token {
+        let token_type = if self.match_char(expected) { if_match } else { or_else };
+        self.make_token(token_type)
     }
 }
